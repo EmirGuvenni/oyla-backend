3dc3a52d13774e7f39f97ff2395cc8fc84609a9f
Emir Güvenni <contact@emirguvenni.com>
2024-03-21 01:33:56 +0300

    feat: add get room details with id

--- src/router/room.ts
+++ src/router/room.ts
@@ -8,3 +8,13 @@ roomRouter.get('/rooms', async (_req, res) => {
   const rooms = coordinator.getRooms();
   return res.json(rooms.map((room) => room.info));
 });
+
+roomRouter.get('/rooms/:id', async (req, res) => {
+  const room = coordinator.getRoom(req.params.id);
+
+  if (!room) {
+    return res.status(404).json({ error: 'Room not found' });
+  }
+
+  return res.json(room.info);
+});
